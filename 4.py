ec6a4d75fb8985737b6a145db084866558188390
Rahul Golani <rahulgolani24@live.com>
2020-05-14 12:00:33 +0530

    4

--- 4.py
+++ 4.py
@@ -1,9 +1,45 @@
+'''
 def getLargestPalindromic():
     result=0
+    #The solution lies between 100 and 999
     for a in range(100,1000):
         for b in range(100,1000):
+            #Checking every Number for palindrome
             if isPalindrome(a*b):
                 result=max(result,a*b)
+    return result
+'''
+
+'''
+The above solution checks numbers twice. This can be optimized if assuming a<=b
+'''
+
+'''
+def getLargestPalindromic():
+    result=0
+    #The solution lies between 100 and 999
+    for a in range(100,1000):
+        for b in range(a,1000):
+            #Checking every Number for palindrome
+            if isPalindrome(a*b):
+                result=max(result,a*b)
+    return result
+'''
+
+
+'''
+Further optimization can be to start from 999 and count up to 100. This wasy we can find the result more quickly
+'''
+
+def getLargestPalindromic():
+    result=0
+    #The solution lies between 100 and 999
+    for a in range(999,99,-1):
+        for b in range(a,99,-1):
+            #Checking every Number for palindrome
+            if isPalindrome(a*b):
+                result=max(result,a*b)
+                break
     return result
 
 def isPalindrome(n):
